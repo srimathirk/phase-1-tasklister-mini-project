5dd99089a4ebbde715ebb63b5dfb6414d5707b30
srimathiradhakrishnan <srimathiradhakrishna@gmail.com>
2023-04-13 16:03:32 -0400

    Completed Lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit',(e)=>{e.preventDefault()
+  handleTask(e.target.querySelector('#new-task-description').value)})
+  form.reset();
 });
+function handleTask(task){
+  let p = document.createElement('p')
+  let btn = document.createElement('button') 
+  btn.addEventListener('click',handleDelete)
+  p.textContent = `${task}  `
+  btn.textContent = 'x'
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector('#list').appendChild(p)
+}
+function handleDelete(e){
+  e.target.parentNode.remove();
+}
